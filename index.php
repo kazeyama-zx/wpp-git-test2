834ea710fd130a7734bcd6511bd34f34be66fe07
kazeyama-zx <kazeyama972@gmail.com>
2025-09-25 15:54:05 +0700

    menambahkan paragraf pada index

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime animi ut quis dolorem iure. Ratione, minima qui obcaecati quis commodi doloribus ea maiores accusamus asperiores quia cum, ullam voluptatum? Excepturi.</p>
 </body>
 </html>
